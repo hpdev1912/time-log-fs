20391a066634a54b2cba857eeda4b4146fcc4c51
hpdev19 <haopham.dev@gmail.com>
2023-11-07 17:23:56 +0700

    update config apply UI and zustand

---
+++ src/components/Logs.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default function Logs() {
+  return (
+    <div>
+      <Table>
+        <TableCaption>List of logs.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-1/3">Date</TableHead>
+            <TableHead className="w-1/3">Hour</TableHead>
+            <TableHead className="w-1/3">Note</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>{new Date().toDateString()}</TableCell>
+            <TableCell>10</TableCell>
+            <TableCell>notes</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
---
+++ src/components/NewLog.tsx
@@ -0,0 +1,123 @@
+'use client';
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+import { GrAdd } from 'react-icons/gr';
+import DatePicker from './DatePicker';
+import { useLogStore } from '@/store';
+import { useToast } from './ui/use-toast';
+import dayjs from 'dayjs';
+
+export default function NewLog() {
+  const log = useLogStore((state) => state.log);
+  const setLog = useLogStore((state) => state.setLog);
+  const setLogs = useLogStore((state) => state.setLogs);
+
+  const { toast } = useToast();
+
+  const validateLog = () => {
+    if (!log.date || !log.hour || log.hour === 0) {
+      throw 'Date or hour can not be empty!';
+    } else if (log.hour >= 24) {
+      throw 'Please enter a valid hour';
+    }
+  };
+
+  const closeDialog = () => {
+    document.getElementById('close-btn')?.click();
+  };
+
+  const handleSubmit = () => {
+    try {
+      validateLog();
+      setLogs(log, dayjs(log.date).format('YYYY-MM-DD'));
+      toast({
+        title: 'Successfully create log',
+        description: `${log.hour} in ${log.date.toDateString()}`,
+      });
+      closeDialog();
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        title: 'Failed to create log',
+        description: error,
+      });
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="w-full sm:w-72 border-dashed border py-3 flex items-center justify-center rounded-md cursor-pointer hover:border-solid">
+          <GrAdd />
+        </div>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create Log</DialogTitle>
+          <DialogDescription>
+            Remember, time is your most valuable asset - invest it wisely with our Time Log App
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="date" className="text-right">
+              date
+            </Label>
+            <DatePicker />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="hour" className="text-right">
+              hour
+            </Label>
+            <Input
+              id="hour"
+              type="number"
+              className="col-span-3"
+              value={log.hour}
+              onChange={(e) =>
+                setLog({
+                  ...log,
+                  hour: parseInt(e.target.value),
+                })
+              }
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="note" className="text-right">
+              Note
+            </Label>
+            <Input
+              id="note"
+              placeholder="note of the log"
+              className="col-span-3"
+              value={log.note}
+              onChange={(e) =>
+                setLog({
+                  ...log,
+                  note: e.target.value,
+                })
+              }
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit" onClick={handleSubmit}>
+            Save
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
